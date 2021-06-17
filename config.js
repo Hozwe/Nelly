f882a11e4b0ff26eb2eb5b5256fc8ea569c3872d
Hozww <78293027+Hozwe@users.noreply.github.com>
2021-06-17 12:49:26 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+
+const config = {
+	// bot
+	token: "token",
+	prefix: "prefix",
+	ownerID: [""],
+	mongodb: "",
+	// channels
+	support: "",
+	bugs: "",
+	// roles
+	developer: "",
+	// emojis
+	downvote: "",
+	upvote: "",
+};
+module.exports = config;
